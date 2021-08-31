eb6984919fde383e97052b93daaf6fdb65a1dce0
Maximiliano Fiandrino <maxi.fiandrino@gmail.com>
2021-08-31 20:00:58 -0300

    Punto 2 finalizado y Punto 3

--- parcialLogicoFiandrino.pl
+++ parcialLogicoFiandrino.pl
@@ -13,6 +13,8 @@ heroe(arthas,55,draenor,humano(paladin,50)).
 heroe(illidan,150,temploOscuro,elfo(250,noche)).
 heroe(jaina,40,dalaran,humano(mago,110)).
 
+esCiudad(Ciudad) :- heroe(_,_,Ciudad,_).
+
 %amigo(Heroe,Amigo).
 amigo(grom,thrall).
 amigo(arthas,jaina).
@@ -20,8 +22,7 @@ amigo(jaina,thrall).
 amigo(jaina,maiev).
 
 % ------------Punto 2-----------
-% esHonorable(Heroe).
-
+% a) esHonorable(Heroe).
 esHonorable(Heroe) :-
     heroe(Heroe,_,_,Raza),
     esHonorableSegunRaza(Raza).
@@ -31,6 +32,45 @@ esHonorableSegunRaza(humano(_,PuntosDeHonor)) :- PuntosDeHonor > 100.
 esHonorableSegunRaza(elfo(CantMagia,_)) :- CantMagia < 100.
 esHonorableSegunRaza(orco(_,chaman,_)).
 esHonorableSegunRaza(orco(_,gladiador,NivelDeFuria)) :- NivelDeFuria < 50.
+
+% b) nivelDeUnHeroe(Heroe,Nivel).
+nivelDeUnHeroe(Heroe,Nivel) :-
+    heroe(Heroe,Exp,_,Raza),
+    nivelSegunRaza(Heroe,Exp,Raza,Nivel).
+
+nivelSegunRaza(Heroe,Exp,humano(_,_),Nivel) :-
+    esHonorable(Heroe),
+    Nivel is Exp * 2.
+
+nivelSegunRaza(Heroe,Exp,humano(_,_),Exp) :-
+    not(esHonorable(Heroe)).
+
+nivelSegunRaza(_,_,elfo(CantMagia,_),CantMagia).
+
+nivelSegunRaza(_,Exp,orco(_,_,NivelDeFuria),Nivel) :- Nivel is Exp - NivelDeFuria.
+
+% ------------Punto 3-----------
+% a) esCiudadHonorable(Ciudad).
+esCiudadHonorable(Ciudad) :-
+    esCiudad(Ciudad),
+    cantDeHeroesHonorables(Ciudad,Cantidad),
+    forall(cantDeHeroesHonorables(_,OtraCantidad) , Cantidad >= OtraCantidad).
+
+cantDeHeroesHonorables(Ciudad,Cantidad) :-
+    esCiudad(Ciudad),
+    findall(Heroe,(viveEn(Heroe,Ciudad),esHonorable(Heroe)),Heroes),
+    length(Heroes, Cantidad).
+
+viveEn(Heroe,Ciudad) :- 
+    heroe(Heroe,_,Ciudad,_).
+
+% b) esCiudadConUnicoHeroe(Ciudad).
+esCiudadConUnicoHeroe(Ciudad) :-
+    viveEn(Heroe,Ciudad),
+    not((viveEn(OtroHeroe,Ciudad) , Heroe \= OtroHeroe)).
+
+% ------------Punto 4-----------
+
 
 
 
